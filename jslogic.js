9a615cfc9a789617536853a3e747cf2ea2e7f3a2
Bhanuprakash <thotabhanu03@gmail.com>
2020-03-02 11:35:18 -0600

    Modified the JsLogic

--- jslogic.js
+++ jslogic.js
@@ -3,16 +3,7 @@ function MaxNumberCalc()
      const First=parseInt(document.querySelector('#FirstNum').value)
      const Second=parseInt(document.querySelector('#SecondNum').value)
 
-     const MaxNum = Math.max(First,Second);
-         
-    // const rTip=rIntialTotal*(rTipPerRate/100)
-    // const rTaxRate=5.50
-    // const rTax=rIntialTotal*(rTaxRate/100)
-    // const rGrandTotal=rIntialTotal+rTip+rTax;
-
-    // document.getElementById("Tip").innerHTML = rTip.toFixed(2);
-    // document.getElementById("Tax").innerHTML = rTax.toFixed(2);
-     //document.getElementById("GrandTotal").innerHTML = rGrandTotal.toFixed(2);
+     const MaxNum = Math.max(First,Second);    
      document.getElementById("MaxNumber").innerHTML = MaxNum;
      
 }
